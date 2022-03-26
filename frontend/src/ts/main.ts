0ea9c4195da0b5e9aa898cbe6cffb5eb69be0291
5kdn <sakuden542+github@gmail.com>
2022-03-26 23:01:47 +0900

    add popup help window

--- frontend/src/ts/main.ts
+++ frontend/src/ts/main.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
+import {popup} from './popup'
 import { createApp } from 'vue'
 import App from './components/app.vue'
 
 const app = createApp(App)
 app.mount('App')
+
+popup()
